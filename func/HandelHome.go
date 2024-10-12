1a7938db063a3a7b01a12e9b6c217fbdf3647945
aelmir <adnane.elmir1@gmail.com>
2024-10-12 11:09:00 +0100

    ff

--- func/HandelHome.go
+++ func/HandelHome.go
@@ -1,25 +1,27 @@
 package groupie
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
 
 type Artist struct {
-	Id int `json:"id"`
-	Name string `json:"name"`
-	Image string `json:"image"`
-	Members []string `json:"members"`
-	CreationDate int `json:"creationDate"`
-	FirstAlbum string `json:"firstAlbum"`
-	Locations string `json:"locations"`
-	ConcertDates string `json:"concertDates"`
-	Relations string `json:"relations"`
+	Id           int      `json:"id"`
+	Name         string   `json:"name"`
+	Image        string   `json:"image"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	Locations    string   `json:"locations"`
+	ConcertDates string   `json:"concertDates"`
+	Relations    string   `json:"relations"`
 }
 
 type Page struct {
 	Code     int
 	MsgError string
+	
 }
 
 var Data = &Page{}
@@ -27,7 +29,7 @@ var Data = &Page{}
 func RenderPage(page string, res http.ResponseWriter) {
 	temp, err := template.ParseFiles("templates/" + page + ".html")
 	if err != nil {
-
+		fmt.Println(err)
 		if page == "error" {
 
 			http.Error(res, "Internal Server Error", 500)
@@ -38,6 +40,7 @@ func RenderPage(page string, res http.ResponseWriter) {
 	}
 	err1 := temp.Execute(res, Data)
 	if err1 != nil {
+		fmt.Println(err1.Error())
 		if page == "error" {
 			http.Error(res, "Internal Server Error", 500)
 			return
@@ -47,13 +50,11 @@ func RenderPage(page string, res http.ResponseWriter) {
 	}
 }
 
-func Error(res http.ResponseWriter, status int, msgerr 
-		fmt.Println(err.Error())string) {
+func Error(res http.ResponseWriter, status int, msgerr string) {
 	Data.MsgError = msgerr
 
 	Data.Code = status
 
-	res.WriteHeader(status)
 	RenderPage("error", res)
 }
 
